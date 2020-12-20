2906581fb4b46d03bcb68547b0c92caea6e6cb33
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-20 18:35:37 +0100

    Create task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+class Task {
+
+    private final String name;
+    private final int points;
+
+    Task(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    int getPoints() {
+        return points;
+    }
+}
