947d8263b091b87579785d70257fc82f3b0b1cbb
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-20 18:35:04 +0100

    Create quiz class

---
+++ src/main/java/Quiz.java
@@ -0,0 +1,14 @@
+class Quiz {
+
+    private final String name;
+    private final int points;
+
+    Quiz(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    int getPoints() {
+        return points;
+    }
+}
