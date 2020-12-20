0b8b4a9e3863e0a499be2bdff135159f5536868f
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-20 18:49:54 +0100

    Create LectureActivity class

---
+++ src/main/java/LectureActivity.java
@@ -0,0 +1,12 @@
+class LectureActivity {
+
+    private final int points;
+
+    LectureActivity(int points) {
+        this.points = points;
+    }
+
+    int getPoints() {
+        return points;
+    }
+}
