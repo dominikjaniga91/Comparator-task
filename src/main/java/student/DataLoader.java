1ec85778dff439e585a5be271f7cfb000f3073ef
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-22 12:20:17 +0100

    Refactor data loader
    Divide code into small methods, add String constants

--- src/main/java/student/DataLoader.java
+++ src/main/java/student/DataLoader.java
@@ -9,20 +9,31 @@ import java.util.stream.Collectors;
 
 class DataLoader {
 
+    private static final String FILE = "students.txt";
+    private static final String WORD_SEPARATOR = ";";
+
     static List<PreAcademyStudent> load() {
+        List<String> lines = readLinesFromFile();
+        return getStudentsList(lines);
+    }
+
+    private static List<String> readLinesFromFile() {
         List<String> lines = Collections.emptyList();
         try {
-            lines = Files.readAllLines(Path.of("students.txt"));
+            lines = Files.readAllLines(Path.of(FILE));
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            System.err.println(ex.getMessage());
         }
+        return lines;
+    }
 
+    private static List<PreAcademyStudent> getStudentsList(List<String> lines) {
         return lines.stream().map(DataLoader::createPreAcademyStudent)
                             .collect(Collectors.toList());
     }
 
     private static PreAcademyStudent createPreAcademyStudent(String line) {
-        String[] data = line.split(";");
+        String[] data = line.split(WORD_SEPARATOR);
 
         String name = data[0];
         String surname = data[1];
