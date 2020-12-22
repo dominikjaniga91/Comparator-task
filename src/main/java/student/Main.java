e4af30c363481c23cb783d86cc21104a2ee9a46f
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-22 15:20:07 +0100

    Invoke run method in main

--- src/main/java/student/Main.java
+++ src/main/java/student/Main.java
@@ -1,19 +1,9 @@
 package student;
 
-import student.strategy.SortingOption;
-
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Sorter sorter = new Sorter();
-        List<PreAcademyStudent> students = DataLoader.load();
-        sorter.sort(students, SortingOption.QUIZ, SortingOption.DESCENDING);
-        DataWriter.write(students);
-
-
+        new Program().run();
     }
-
 }
