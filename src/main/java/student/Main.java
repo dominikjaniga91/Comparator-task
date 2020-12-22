f95e21fc3c809d342c61ec0846c87f8311a23b25
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-22 12:08:56 +0100

    Use Sorter and DataWriter in main class

--- src/main/java/student/Main.java
+++ src/main/java/student/Main.java
@@ -1,64 +1,19 @@
 package student;
 
+import student.strategy.SortingOption;
+
 import java.util.List;
-import static java.util.Comparator.*;
-import static java.lang.System.out;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Sorter sorter = new Sorter();
         List<PreAcademyStudent> students = DataLoader.load();
+        sorter.sort(students, SortingOption.QUIZ, SortingOption.DESCENDING);
+        DataWriter.write(students);
 
 
-        out.println("unsorted list");
-        print(students);
-
-        out.println("list sorted ascending by quizzes points ");
-        students.sort(comparingInt(PreAcademyStudent::quizPoints));
-        print(students);
-
-
-        out.println("list sorted descending by quizzes points ");
-        students.sort(comparingInt(PreAcademyStudent::quizPoints).reversed());
-        print(students);
-
-
-        out.println("list sorted ascending by tasks points ");
-        students.sort(comparingInt(PreAcademyStudent::taskPoints));
-        print(students);
-
-
-        out.println("list sorted descending by tasks points ");
-        students.sort(comparingInt(PreAcademyStudent::taskPoints).reversed());
-        print(students);
-
-        out.println("list sorted ascending by lectures activities points ");
-        students.sort(comparingInt(PreAcademyStudent::activityPoints));
-        print(students);
-
-
-        out.println("list sorted descending by lectures activities points ");
-        students.sort(comparingInt(PreAcademyStudent::activityPoints).reversed());
-        print(students);
-
-
-        out.println("list sorted ascending by total points ");
-        students.sort(naturalOrder());
-        print(students);
-
-
-        out.println("list sorted descending by total points ");
-        students.sort(reverseOrder());
-        print(students);
-    }
-
-
-
-    private static void print(List<PreAcademyStudent> list) {
-        out.println("Name Surname Quizzes Tasks Activities Total");
-        list.forEach(out::println);
-        out.println();
     }
 
 }
