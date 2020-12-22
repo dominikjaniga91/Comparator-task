d009392e3486c1cabba9a493564c08e8df8f5789
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-22 15:04:38 +0100

    Refactor data writer

--- src/main/java/student/DataWriter.java
+++ src/main/java/student/DataWriter.java
@@ -1,29 +1,39 @@
 package student;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.nio.charset.StandardCharsets.UTF_16;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+
 class DataWriter {
 
-    static void write( List<PreAcademyStudent> students) {
-        List<String> lines = students.stream()
-                                    .map(PreAcademyStudent::toString)
-                                    .collect(Collectors.toList());
-        try {
-            Files.write(Path.of("sorted.txt"),
-                        lines,
-                        StandardCharsets.UTF_16,
-                        StandardOpenOption.TRUNCATE_EXISTING);
+    private static final Path PATH = Path.of("sorted.txt");
+    private static final String HEADER = "Name\tSurname\tQuizzes\tTasks\tActivities\tTotal\n";
 
+    static void write(List<PreAcademyStudent> students) {
+        List<String> lines = getStringList(students);
+        writeHeader();
+        try {
+            Files.write(PATH, lines, UTF_16, APPEND);
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private static List<String> getStringList(List<PreAcademyStudent> students) {
+        return students.stream()
+                .map(PreAcademyStudent::toString)
+                .collect(Collectors.toList());
+    }
+
+    static void writeHeader() {
+        try(var writer = Files.newBufferedWriter(PATH, UTF_16, TRUNCATE_EXISTING)) {
+            writer.write(HEADER);
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
         }
