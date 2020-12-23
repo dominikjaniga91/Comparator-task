ea297400fcffea32cba2dec01f01bc0b479bd54f
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-23 12:46:29 +0100

    Rename Selector to ComparatorSelector

--- src/main/java/student/comparator/strategy/ComparatorSelector.java
+++ src/main/java/student/comparator/strategy/ComparatorSelector.java
@@ -2,7 +2,7 @@ package student.comparator.strategy;
 
 import student.comparator.enums.Category;
 
-public class Selector {
+public class ComparatorSelector {
 
     public static StudentComparator select(Category sorter) {
         return switch (sorter) {
