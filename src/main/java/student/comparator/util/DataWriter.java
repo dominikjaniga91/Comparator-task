84c2ed8748ef04979f0118411e5458c4ed5e5b66
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-23 13:29:00 +0100

    Refactor code in DataWriter class
    Add file name, change character encoding,
    set uo creation a new file every time a method is invoke

--- src/main/java/student/comparator/util/DataWriter.java
+++ src/main/java/student/comparator/util/DataWriter.java
@@ -1,27 +1,28 @@
 package student.comparator.util;
 
 import student.comparator.PreAcademyStudent;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.nio.charset.StandardCharsets.UTF_16;
-import static java.nio.file.StandardOpenOption.APPEND;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.*;
 
 public class DataWriter {
 
-    private static final Path PATH = Path.of("sorted.txt");
+    private static final String FILE_NAME = "sorted_students_" + LocalDateTime.now() + ".csv";
+    private static final Path PATH = Paths.get(FILE_NAME);
     private static final String HEADER = "Name\tSurname\tQuizzes\tTasks\tActivities\tTotal\n";
 
     public static void write(List<PreAcademyStudent> students) {
         List<String> lines = getStringList(students);
         writeHeader();
         try {
-            Files.write(PATH, lines, UTF_16, APPEND);
+            Files.write(PATH, lines, UTF_8, APPEND);
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
         }
@@ -34,7 +35,7 @@ public class DataWriter {
     }
 
     private static void writeHeader() {
-        try(var writer = Files.newBufferedWriter(PATH, UTF_16, TRUNCATE_EXISTING)) {
+        try(var writer = Files.newBufferedWriter(PATH, UTF_8, CREATE_NEW)) {
             writer.write(HEADER);
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
