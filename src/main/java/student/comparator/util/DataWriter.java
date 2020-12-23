98ff0899ebe6400e7f885dff25f9581cad65a9fc
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-23 16:37:07 +0100

    Change the way of writing data
    Use newBufferedReader instead of Files.write

--- src/main/java/student/comparator/util/DataWriter.java
+++ src/main/java/student/comparator/util/DataWriter.java
@@ -1,6 +1,7 @@
 package student.comparator.util;
 
 import student.comparator.PreAcademyStudent;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,40 +9,42 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 public class DataWriter {
 
-    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm:ss");
-    private static final String FILE_NAME = "sorted_students_" + LocalDateTime.now().format(format) + ".csv";
-    private static final Path PATH = Paths.get(FILE_NAME);
     private static final String HEADER = "Name\tSurname\tQuizzes\tTasks\tActivities\tTotal\n";
 
     public void write(List<PreAcademyStudent> students) {
-        List<String> lines = getStringList(students);
-        writeHeader();
-        try {
-            Files.write(PATH, lines, UTF_8, APPEND);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-        }
-    }
+        String fileName = createFileName();
+        Path path = Paths.get(fileName);
 
-    private List<String> getStringList(List<PreAcademyStudent> students) {
-        return students.stream()
-                .map(PreAcademyStudent::toString)
-                .collect(Collectors.toList());
-    }
-
-    private void writeHeader() {
-        try(var writer = Files.newBufferedWriter(PATH, UTF_8, CREATE_NEW)) {
+        try(var writer = Files.newBufferedWriter(path, CREATE_NEW)) {
             writer.write(HEADER);
+            writeStudentsList(students, writer);
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
         }
+    }
+
+    private void writeStudentsList(List<PreAcademyStudent> students, BufferedWriter writer) {
+        students.stream().map(PreAcademyStudent::toString)
+                .forEach(student -> writeStudent(writer, student));
+    }
+
+    private void writeStudent(BufferedWriter writer, String student) {
+        try {
+            writer.write(student);
+            writer.newLine();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private String createFileName() {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm:ss");
+        String dateTime = LocalDateTime.now().format(format);
+        return "sorted_students_" + dateTime + ".csv";
     }
 }
