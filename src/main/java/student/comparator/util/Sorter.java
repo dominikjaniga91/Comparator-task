ba8313b7ee3401cac2ced625480f418c20332b87
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-23 14:39:15 +0100

    Rename class in Sorter

--- src/main/java/student/comparator/util/Sorter.java
+++ src/main/java/student/comparator/util/Sorter.java
@@ -4,13 +4,15 @@ import student.comparator.PreAcademyStudent;
 import student.comparator.enums.Category;
 import student.comparator.enums.Option;
 import student.comparator.strategy.*;
+import student.comparator.strategy.decorator.DescendingDecorator;
+
 import java.util.List;
 
 public class Sorter {
 
     public static void sort(List<PreAcademyStudent> list, Category category, Option sortingOption) {
 
-        StudentComparator comparator = Selector.select(category);
+        StudentComparator comparator = ComparatorSelector.select(category);
         if (sortingOption == Option.DESCENDING) {
             StudentComparator decorated = new DescendingDecorator(comparator);
             list.sort(decorated.get());
